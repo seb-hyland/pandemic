483e20ed1b6ff789a6e609f3e8d819d31322ba40
Sebastian Hyland <st.hyland05@gmail.com>
2025-12-21 20:24:04 -0800

    feat: variable window size, web

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,111 @@
-use std::{
-    f32::{self, consts::PI},
-    time::Instant,
-};
+use std::f32::{self, consts::PI};
+use web_time::{Duration, Instant};
 
-use eframe::{App, NativeOptions};
+use eframe::App;
 use egui::{
-    Color32, Pos2, Shape, Stroke, Ui, Vec2, ViewportBuilder,
+    Button, Color32, Grid, Label, Pos2, Shape, Slider, Stroke, Ui, Vec2, ViewportBuilder,
     ahash::{HashMap, HashMapExt},
     epaint::CircleShape,
 };
 use rand::{random_bool, random_range};
 
+#[cfg(not(target_arch = "wasm32"))]
+fn main() {
+    let native_options = eframe::NativeOptions {
+        viewport: ViewportBuilder::default()
+            .with_inner_size(Vec2 { x: 800., y: 700. })
+            .with_resizable(true),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "pandemic",
+        native_options,
+        Box::new(|_| Ok(Box::new(Pandemic::new(3, 500)))),
+    )
+    .unwrap();
+}
+
+#[cfg(target_arch = "wasm32")]
+fn main() {
+    use eframe::wasm_bindgen::JsCast as _;
+
+    let web_options = eframe::WebOptions::default();
+
+    wasm_bindgen_futures::spawn_local(async {
+        let document = web_sys::window()
+            .expect("No window")
+            .document()
+            .expect("No document");
+
+        let canvas = document
+            .get_element_by_id("the_canvas_id")
+            .expect("Failed to find the_canvas_id")
+            .dyn_into::<web_sys::HtmlCanvasElement>()
+            .expect("the_canvas_id was not a HtmlCanvasElement");
+
+        let start_result = eframe::WebRunner::new()
+            .start(
+                canvas,
+                web_options,
+                Box::new(|_| Ok(Box::new(Pandemic::new(3, 500)))),
+            )
+            .await;
+
+        // Remove the loading text and spinner:
+        if let Some(loading_text) = document.get_element_by_id("loading_text") {
+            match start_result {
+                Ok(_) => {
+                    loading_text.remove();
+                }
+                Err(e) => {
+                    loading_text.set_inner_html(
+                        "<p> The app has crashed. See the developer console for details. </p>",
+                    );
+                    panic!("Failed to start eframe: {e:?}");
+                }
+            }
+        }
+    });
+}
+
 struct Pandemic {
+    // Initial values
+    init_infected: usize,
+    total: usize,
+
     // Public params
     infection_prob: f32,
-    infection_time: f32,
+    infection_time_s: f32,
     death_prob: f32,
     step_speed: f32,
+    paused: bool,
 
     // Data
     grid: SpatialGrid,
     last_frame_time: Instant,
+    time_elapsed: Duration,
+}
+
+impl App for Pandemic {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::TopBottomPanel::bottom("control_panel")
+            .exact_height(400.)
+            .show(ctx, |ui| {
+                egui::SidePanel::left("params")
+                    .exact_width(250.)
+                    .show_inside(ui, |ui| self.params_ui(ui));
+            });
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            self.grid.render(ui);
+            if !self.paused {
+                self.step();
+            }
+        });
+
+        // Re-render (hot loop)
+        ctx.request_repaint();
+    }
 }
 
 const X_MAX: i32 = 160;
@@ -31,26 +116,91 @@ const Y_MAX_FLOAT: f32 = Y_MAX as f32;
 impl Pandemic {
     fn new(infected: usize, total: usize) -> Self {
         Self {
+            init_infected: infected,
+            total,
+
             infection_prob: 0.5,
-            infection_time: 14000.0,
+            infection_time_s: 14.0,
             death_prob: 0.1,
-            step_speed: 2.0,
+            step_speed: 1.0,
+            paused: false,
 
             grid: SpatialGrid::new_with_capacity(infected, total),
             last_frame_time: Instant::now(),
+            time_elapsed: Duration::ZERO,
         }
     }
 
+    fn params_ui(&mut self, ui: &mut Ui) {
+        ui.add_space(20.);
+
+        ui.heading("Controls");
+        Grid::new("playback_keys").show(ui, |ui| {
+            if ui.add(Button::new("⏸")).clicked() {
+                self.paused = true;
+            };
+            if ui.add(Button::new("▶")).clicked() {
+                self.step_speed = 1.0;
+                self.paused = false;
+                self.last_frame_time = Instant::now();
+            };
+            if ui.add(Button::new("▶▶")).clicked() {
+                self.step_speed = 2.0;
+                self.paused = false;
+                self.last_frame_time = Instant::now();
+            };
+            if ui.add(Button::new("▶▶▶")).clicked() {
+                self.step_speed = 4.0;
+                self.paused = false;
+                self.last_frame_time = Instant::now();
+            };
+            if ui.add(Button::new("⟳")).clicked() {
+                *self = Self::new(self.init_infected, self.total);
+            }
+        });
+        ui.add_space(20.);
+
+        ui.heading("Initial conditions");
+        ui.add(Label::new("Initial infected"));
+        ui.add(Slider::new(&mut self.init_infected, 0..=100000));
+
+        ui.add(Label::new("Total people"));
+        ui.add(Slider::new(&mut self.total, 0..=100000));
+        ui.add_space(20.);
+
+        ui.heading("Simulation parameters");
+        ui.add(Label::new("Death probability"));
+        ui.add(Slider::new(&mut self.death_prob, 0.0..=1.0));
+
+        ui.add(Label::new("Infection probability"));
+        ui.add(Slider::new(&mut self.infection_prob, 0.0..=1.0));
+
+        ui.add(Label::new("Infection time (days)"));
+        ui.add(Slider::new(&mut self.infection_time_s, 0.0..=30.0));
+        ui.add_space(20.);
+
+        ui.add(Label::new(format!(
+            "Current time: {:.1} days",
+            self.time_elapsed.as_secs_f32()
+        )));
+    }
+
     fn step(&mut self) {
-        let frame_time = self.last_frame_time.elapsed().as_millis() as f32 * self.step_speed;
+        // Amount of motion per ms
+        const MOVE_AMOUNT: f32 = 0.01;
+
+        let elapsed = self.last_frame_time.elapsed();
+        let frame_time = elapsed.as_millis() as f32 * self.step_speed;
+        self.time_elapsed += elapsed;
         self.last_frame_time = Instant::now();
 
-
+        let infection_time = self.infection_time_s * 1000.0;
         let survival_prob = 1.0 - self.death_prob;
-        let survive_this_frame =
-            survival_prob.powf(frame_time / self.infection_time) as f64;
+        let survive_this_frame = survival_prob.powf(frame_time / infection_time) as f64;
         let infection_prob = 1.0 - self.infection_prob;
-        let not_infected_this_frame = infection_prob.powf(frame_time / 75.0) as f64;
+        // Somewhat bastardized estimation
+        let not_infected_this_frame =
+            infection_prob.powf(frame_time / ((1.0 / MOVE_AMOUNT) * 0.85)) as f64;
 
         // Iterate over rows and cols
         for x_pos in 0..X_MAX {
@@ -59,7 +209,7 @@ impl Pandemic {
                 // Get everyone in grid element
                 if let Some(people) = self.grid.0.get_mut(&(x_pos, y_pos)) {
                     // Step each individual
-                    let dist_to_move = 0.01 * frame_time;
+                    let dist_to_move = MOVE_AMOUNT * frame_time;
                     people_to_move.extend(people.extract_if(.., |person| {
                         // Step direction
                         let pos = &mut person.pos;
@@ -94,7 +244,7 @@ impl Pandemic {
 
                             // Update infection time
                             let new_infection_time = t + frame_time;
-                            person.state = if new_infection_time > self.infection_time {
+                            person.state = if new_infection_time > infection_time {
                                 InfectionState::Recovered
                             } else {
                                 InfectionState::Infected(new_infection_time)
@@ -113,10 +263,7 @@ impl Pandemic {
                         .any(|person| matches!(person.state, InfectionState::Infected(_)));
                     if contains_infected {
                         for person in people {
-                            match (
-                                person.state,
-                                random_bool(1.0 - not_infected_this_frame),
-                            ) {
+                            match (person.state, random_bool(1.0 - not_infected_this_frame)) {
                                 (InfectionState::Healthy, true) => {
                                     person.state = InfectionState::Infected(0.0)
                                 }
@@ -179,11 +326,31 @@ impl SpatialGrid {
     }
 
     fn render(&self, ui: &mut Ui) {
+        const TARGET_RATIO: f32 = 16.0 / 10.0;
+        let avail = ui.available_size() - Vec2 { x: 10.0, y: 10.0 };
+
+        let aspect_ratio = avail.x / avail.y;
+
+        let (x, y, x_off, y_off) = if aspect_ratio > TARGET_RATIO {
+            // x is too large
+            let target_x = avail.y * 1.6;
+            let x_off = (avail.x - target_x) / 2.0;
+            (target_x, avail.y, x_off, 0.0)
+        } else if aspect_ratio > TARGET_RATIO {
+            // y is too large
+            let target_y = avail.x / 1.6;
+            let y_off = (avail.y - target_y) / 2.0;
+            (avail.x, target_y, 0.0, y_off)
+        } else {
+            (avail.x, avail.y, 0.0, 0.0)
+        };
+        let (x_ratio, y_ratio) = (x / 160.0, y / 100.0);
+
         ui.painter().extend(self.0.values().flatten().map(|person| {
             Shape::Circle(CircleShape {
                 center: Pos2 {
-                    x: person.pos.x * 10.,
-                    y: person.pos.y * 10.,
+                    x: x_off + 5.0 + person.pos.x * x_ratio,
+                    y: y_off + 5.0 + person.pos.y * y_ratio,
                 },
                 radius: 5.0,
                 fill: match person.state {
@@ -213,30 +380,4 @@ enum InfectionState {
     Infected(f32),
     Recovered,
     Dead,
-}
-
-impl App for Pandemic {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            self.grid.render(ui);
-            self.step();
-        });
-
-        ctx.request_repaint();
-    }
-}
-
-fn main() {
-    let native_options = NativeOptions {
-        viewport: ViewportBuilder::default()
-            .with_inner_size(Vec2 { x: 1600., y: 1000. })
-            .with_resizable(false),
-        ..Default::default()
-    };
-    eframe::run_native(
-        "pandemic",
-        native_options,
-        Box::new(|_| Ok(Box::new(Pandemic::new(3, 500)))),
-    )
-    .unwrap();
 }
